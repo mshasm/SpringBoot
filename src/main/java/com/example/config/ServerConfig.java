905fa701cfec38c808dad1d8f8595e4707d12bd3
mshasm <mshasm26@gmail.com>
2025-03-23 22:21:40 +0530

    Delete src directory

--- src/main/java/com/example/config/ServerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ServerConfig {
-    // You can define server-related configurations here
-}
-
